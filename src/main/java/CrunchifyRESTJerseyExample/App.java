87ded5b16222cfa7ea8822aa2179c7e6c7b928dd
Klevedko <31962820+Klevedko@users.noreply.github.com>
2018-06-06 22:06:57 +0300

    Initial commit

---
+++ src/main/java/CrunchifyRESTJerseyExample/App.java
@@ -0,0 +1,10 @@
+package main.java.CrunchifyRESTJerseyExample;
+
+/**
+ * Created by BuyMe on 06.06.2018.
+ */
+public class App {
+    public static void main(String[] args) {
+        CtoFService ctoFService = new CtoFService();
+    }
+}
